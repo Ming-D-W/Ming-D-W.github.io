a8f1005587d0d520a9f4f7097620097768146ed3
Ming-D-W <wuwanming0405@163.com>
2023-03-30 19:36:27 +0800

    更新内容 + Action 自动部署

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 # 本脚本为自动部署的入口脚本，只需执行该脚本，即可实现 GitHub 的自动部署，以及 GitHub 的仓库同步到 Gitee 中，最后 Gitee 将仓库同步到到服务器
 
-GITHUB_REPO=git@github.com:Kele-Bingtang/Kele-Bingtang.github.io.git
+GITHUB_REPO=git@github.com:Ming-D-W/Ming-D-W.github.io.git
 
 # GITEE_REPO=git@gitee.com:kele-bingtang/Kele-Bingtang.git
 
@@ -16,7 +16,7 @@ git add -A
 
 git commit -m "${comment}"    # $1 是启动该脚本传来的参数，如 sh push.sh hello，其中 hello 就会插入到 $1 处，如果想两个参数，则加 $2
 
-git push $GITHUB_REPO   
+git push $GITHUB_REPO
 
 # git push $GITEE_REPO
 
