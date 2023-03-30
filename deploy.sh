dc49916c7a9bb617505a56676a3219aeae82b07e
Ming-D-W <wuwanming0405@163.com>
2023-03-30 19:56:24 +0800

    111

--- deploy.sh
+++ deploy.sh
@@ -3,47 +3,22 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+
+push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=gh-pages # 推送的分支
+
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd $dist_path
 
-# 如果发布到自定义域名，请使用，不填则使用 GitHub 自带的 <username>.github.io
-#echo 'github.youngkbt.cn' > CNAME
-
-# 如果手运行该脚本，则执行 if 里的，如果是 GitHub 自动执行该脚本，则是 else 里的
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy 脚本部署'
-  githubUrl=git@github.com:Ming-D-W/Ming-D-W.github.io.git
-  # giteeUrl=git@gitee.com:kele-bingtang/Kele-Bingtang.git
-else
-  msg='来自 Github actions 的自动部署'
-  githubUrl=https://Ming-D-W:${GITHUB_TOKEN}@github.com/Ming-D-W/Ming-D-W.github.io.git
-  git config --global user.name "Ming-D-W"
-  git config --global user.email "wuwanming0405@163.com"
-fi
 git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到 Github gh-pages 分支
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
 
-# git push -f $giteeUrl master:gh-pages # 推送到 Gitee gh-pages 分支
-
-# deploy to coding pages
-# echo 'www.YoungKbt.cn\YoungKbt.cn' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
-
-# 退回开始所在目录
 cd -
-# 删除打包项目
-rm -rf docs/.vuepress/dist
+rm -rf $dist_path
