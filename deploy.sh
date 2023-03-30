bf891ad96ab49867fc3509ccd0132dec1cf1659f
吴万明 <wuwanming0405@163.com>
2023-03-30 22:28:19 +0800

    更新内容 + Action 自动部署

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,8 @@ cd docs/.vuepress/dist
 # 如果手运行该脚本，则执行 if 里的，如果是 GitHub 自动执行该脚本，则是 else 里的
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy 脚本部署'
-  githubUrl=git@github.com:Ming-D-W/Ming-D-W.github.io.git
+  githubUrl=`git remote get-url --push origin`
+#  githubUrl=git@github.com:Ming-D-W/Ming-D-W.github.io.git
   # giteeUrl=git@gitee.com:kele-bingtang/Kele-Bingtang.git
 else
   msg='来自 Github actions 的自动部署'
