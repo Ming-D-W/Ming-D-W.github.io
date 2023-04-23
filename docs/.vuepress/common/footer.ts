aa6b849634a2d28e4bcd4004ccc1f74c5ff1e308
Ming-D-W <wuwanming0405@163.com>
2023-04-23 21:33:38 +0800

    更新内容 + Action 自动部署

--- docs/.vuepress/common/footer.ts
+++ docs/.vuepress/common/footer.ts
@@ -11,6 +11,6 @@ export default <Footer> {
   copyrightInfo:
     penName + ' | ' + footerTitle + '<br>' +
     '<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33019202000592" target="_blank" style="margin-right: 20px; height:20px;line-height:20px;"><img src="https://photo-album-1314189846.cos.ap-shanghai.myqcloud.com/202304131844613.png" style="height:18px; margin: 0px 5px 0px 0px;"/>浙公网安备 33019202000592号</a>'+
-      ' <a href="http://beian.miit.gov.cn/" target="_blank">陇ICP备2023000815号</a>',
+      ' <a href="http://beian.miit.gov.cn/" target="_blank">陇ICP备2023000815号-1</a>',
 }
 
